9cdf87a338bba4a7d2e80aaaccb0be24238e3177
Feb17 <kira.gyz90@gmail.com>
2021-11-05 16:48:43 +0800

    Update 石头剪刀布游戏.py

--- 石头剪刀布游戏.py
+++ 石头剪刀布游戏.py
@@ -1,33 +1,41 @@
+
 import random
 
+'''
+class numExceptionError(Exception):
+    def __init__(self, ErrorInfo="请输入0 - 2 之间的一个数字"):
+        super().__init__(self) # 初始化父类
+        self.errorinfo=ErrorInfo
+    def __str__(self):
+        print(self.errorinfo)
+        #return self.errorinfo
+'''
+
 def main():
-    a = 0
-    x = random.randint(0,2)
+    a = -1
     while True:
         try:
-            if a != 0 and a != 1 and a !=2:
-                a = int(input("请输入： 剪刀（0），石头（1）， 布（2）: "))
-        except ValueError:
-                print("请输入0 - 2 之间的一个数字！")
-                continue
-        except IndexError:
-                print("请输入0 - 2 之间的一个数字！")
-        if a == x:
-            print("平局")
-        elif a == 0 and x == 1:
-            print("你输啦")
-        elif a == 0 and x == 2:
-            print("你赢啦")
-        elif a == 1 and x == 0:
-            print("你赢啦")
-        elif a == 1 and x == 2:
-            print("你输啦")
-        elif a == 2 and x == 0:
-            print("你输啦")
-        elif a == 2 and x == 1:
-            print("你赢啦")
+            a = int(input("请输入： 剪刀（0），石头（1）， 布（2）: "))
+            if a < 0 or a > 2:
+                print("请输入0 - 2 之间的一个数字")
+        except Exception as e:
+            print("请输入0 - 2 之间的一个数字")
         else:
-            print("请输入0 - 2 之间的一个数字！")
+            x = random.randint(0,2)
+            if a == x:
+                print("平局")
+            elif a == 0 and x == 1:
+                print("你输啦")         
+            elif a == 0 and x == 2:
+                print("你赢啦")
+            elif a == 1 and x == 0:
+                print("你赢啦")
+            elif a == 1 and x == 2:
+                print("你输啦")
+            elif a == 2 and x == 0:
+                print("你输啦")
+            elif a == 2 and x == 1:
+                print("你赢啦")
 
 if __name__ == '__main__':
     main()
