3d3f6c6c69e0826a63abf906070d200c1392741f
feb17 <kira.gyz90@gmail.com>
2022-12-16 14:04:04 +0800

    update

---
+++ Chapter 4. 高级变量/hm_05_元组的基本使用.py
@@ -0,0 +1,13 @@
+info_tuple = ("张三", 18, 1.75)
+
+# 1  取值
+print(info_tuple[0])
+
+# 2. 取索引
+print(info_tuple.index("张三"))
+
+# 3. 统计计数
+print(info_tuple.count(18))
+
+# 4. 统计元组数据数量（长度）
+print(len(info_tuple))
