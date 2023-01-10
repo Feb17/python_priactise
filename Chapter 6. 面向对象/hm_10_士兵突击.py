796d205e1183ee8ac0ff5145a4cb294abd7b9b4d
feb17 <kira.gyz90@gmail.com>
2023-01-10 10:59:25 +0800

    update

--- Chapter 6. 面向对象/hm_10_士兵突击.py
+++ Chapter 6. 面向对象/hm_10_士兵突击.py
@@ -5,6 +5,7 @@ class Gun:
 
     def add_bullet(self, count):
         self.bullet_count += count
+        print("已装填[%d发]子弹" % count)
 
     def shoot(self):
         if self.bullet_count <= 0:
@@ -19,9 +20,26 @@ class Soldier:
         self.name = name
         self.gun = None
 
+    def fire(self):
+        # 1. 判断士兵是否有枪
+        if self.gun is None:
+            return "%s没有枪，无法射击" % self.name
+
+        # 2. 喊口号
+        print("冲啊")
+
+        # 3. 让枪装填子弹
+        # Ak47.add_bullet(50)
+        self.gun.add_bullet(50)
+
+        # 4. 让枪发射子弹
+        self.gun.shoot()
+
 
 # 1. 创建枪对象
-ak47 = Gun("AK47")
+Ak47 = Gun("AK47")
 
-ak47.add_bullet(50)
-ak47.shoot()
+# 2. 创建士兵对象
+XuSanDuo = Soldier("许三多")
+XuSanDuo.gun = Ak47
+XuSanDuo.fire()
